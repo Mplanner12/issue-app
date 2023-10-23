ef603d34849d4faa091c0561df906f447f7cf154
Mplanner12 <almussanplanner12@gmail.com>
2023-10-23 17:54:10 +0100

    replaced textarea with a text editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,22 @@
 "use client";
-import { TextField } from "@radix-ui/themes";
-import { TextArea } from "@radix-ui/themes";
-import { Button } from "@radix-ui/themes";
 import React from "react";
+import { TextField } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+// import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
+  const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+    ssr: false,
+  });
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root>
         <TextField.Slot></TextField.Slot>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
